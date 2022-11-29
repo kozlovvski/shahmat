036fc50c7e70ac0229d0c6044414ee219ee79eda
Michał Kozłowski <michal.kozlowski@housecallpro.com>
2022-11-29 09:13:04 +0100

    bump node version back to 14

--- packages/fen/src/lib/decodeFEN/index.test.ts
+++ packages/fen/src/lib/decodeFEN/index.test.ts
@@ -1,5 +1,5 @@
 import { ShahmatBoardState } from "@shahmat/utils";
-import { decodeFEN } from ".";
+import { decodeFEN } from "@shahmat/fen";
 
 describe("decodeFEN", () => {
   it("throws when argument is not a string", () => {
