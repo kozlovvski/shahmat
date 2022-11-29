d932fa60c39656a8e374749b76cf34c4e68d271b
Michał Kozłowski <michal@kozlovv.ski>
2022-11-30 00:07:05 +0100

    adjust tests to new config

--- jest.config.js
+++ jest.config.js
@@ -4,11 +4,12 @@ const projects = projectNames.map((displayName) => ({
   preset: "ts-jest",
   testEnvironment: "node",
   displayName,
-  testMatch: [`<rootDir>/packages/${displayName}/src/**/*.test.ts`],
+  testMatch: [`<rootDir>/packages/${displayName}/src/**/*.test.{ts,tsx}`],
 }));
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   verbose: true,
   projects,
+  collectCoverageFrom: [`<rootDir>/packages/*/src/**/!(*.test).{ts,tsx}`],
 };
